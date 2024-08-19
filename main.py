d9021a6867f3f2a2f698d6609fb6f90093c103fb
oblivion4us <nft4oblivion@gmail.com>
2024-08-19 15:09:49 +0200

    add new attributes

---
+++ main.py
@@ -0,0 +1,7 @@
+class Task():
+    def __init__(self, task_description, due_date, status):
+        self.task_description = task_description
+        self.due_date = due_date
+        self.status = status
+
+    def add_new_task(self):
