c9d8d89271c2dea53be14921416383cca518e79f
oblivion4us <nft4oblivion@gmail.com>
2024-08-20 16:03:10 +0200

    update main and ending of program

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-class Task():
-    def __init__(self, task_description, due_date, status):
-        self.task_description = task_description
-        self.due_date = due_date
-        self.status = status
+from datetime import datetime
 
-    def add_new_task(self):
-        pass
+class Task:
+    def __init__(self, description, due_date):
+        self.description = description
+        self.due_date = due_date
+        self.is_completed = False
+        self.tasks = []
+
+    def add_task(self, description, due_date):
+        task = {
+            'description': description,
+            'due_date': due_date,
+            'is_completed': False
+        }
+        self.tasks.append(task)
+        print(f"Задача '{description}' добавлена.")
+
+    def mark_task_completed(self, description):
+        for task in self.tasks:
+            if task['description'] == description:
+                task['is_completed'] = True
+                print(f"Задача '{description}' отмечена как выполненная.")
+                return
+        print(f"Задача '{description}' не найдена.")
+
+    def get_pending_tasks(self):
+        pending_tasks = [task for task in self.tasks if not task['is_completed']]
+        print("Список текущих (не выполненных) задач:")
+        for task in pending_tasks:
+            due_date = task['due_date']
+            print(f"Задача: {task['description']}. Срок: {due_date}")
